c4dbb1808f4ac3006d166fa6d4d640e6295d58a3
Emmanuel Omondi Aloo <aloo25emmanuel@gmail.com>
2022-06-09 20:39:09 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(someValue= "roller-skate"){
+    
+       return `This Saturday, I want to ${someValue}!`;
+    }
+
+
+
+    function mondayWork(someValue="go to the office"){
+        return `This Monday, I will ${someValue}.`
+    }
+
+
+    function wrapAdjective(results="*"){
+        return function(emphatic="a hard worker") {
+            return`You are ${results}${emphatic}${results}!`
+
+        }
+    }
